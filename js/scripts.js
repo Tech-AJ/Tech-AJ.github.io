70efc29e13d0822ad9d80bfda3b3858f194168c3
Apoorva Jain <jainapoorva1111@gmail.com>
2021-06-21 12:24:20 +0530

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+var carousel = $(".carousel"),
+    currdeg = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e) {
+    if (e.data.d == "n") {
+        currdeg = currdeg - 60;
+    }
+    if (e.data.d == "p") {
+        currdeg = currdeg + 60;
+    }
+    carousel.css({
+        "-webkit-transform": "rotateY(" + currdeg + "deg)",
+        "-moz-transform": "rotateY(" + currdeg + "deg)",
+        "-o-transform": "rotateY(" + currdeg + "deg)",
+        "transform": "rotateY(" + currdeg + "deg)"
+    });
+}
+
+
+
+function autoRotate() {
+    console.log("ar");
+    var timer;
+
+    timer = setInterval(function() {
+        currdeg = currdeg - 60;
+        console.log("ar2");
+        carousel.css({
+            "-webkit-transform": "rotateY(" + currdeg + "deg)",
+            "-moz-transform": "rotateY(" + currdeg + "deg)",
+            "-o-transform": "rotateY(" + currdeg + "deg)",
+            "transform": "rotateY(" + currdeg + "deg)"
+        });
+    }, 3000);
+
+}
+
+$(document).ready(function() {
+
+        console.log("ready");
+        autoRotate();
+        console.log("ready2");
+    }
+
+);
